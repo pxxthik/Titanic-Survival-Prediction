e543deec75d290c2d9fd985e0a99831e50441490
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2025-06-19 16:28:21 +0530

    dvc: Added model registry stage with mlflow

---
+++ src/models/register_model.py
@@ -0,0 +1,75 @@
+import json
+import mlflow
+import os
+from dotenv import load_dotenv
+
+import src.utils as utils
+
+logger = utils.configure_logger(__name__, log_file="register_model.log")
+
+
+# Set up DagsHub credentials for MLflow tracking
+load_dotenv()
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "pxxthik"
+repo_name = "Titanic-Survival-Prediction"
+
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+
+
+def load_model_info(file_path: str) -> dict:
+    """Load the model info from a JSON file."""
+    try:
+        with open(file_path, 'r') as file:
+            model_info = json.load(file)
+        logger.debug('Model info loaded from %s', file_path)
+        return model_info
+    except FileNotFoundError:
+        logger.error('File not found: %s', file_path)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error occurred while loading the model info: %s', e)
+        raise
+
+def register_model(model_name: str, model_info: dict):
+    """Register the model to the MLflow Model Registry."""
+    try:
+        model_uri = f"runs:/{model_info['run_id']}/{model_info['model_name']}"
+        
+        # Register the model
+        model_version = mlflow.register_model(model_uri, model_name)
+        
+        # Transition the model to "Staging" stage
+        client = mlflow.tracking.MlflowClient()
+        client.transition_model_version_stage(
+            name=model_name,
+            version=model_version.version,
+            stage="Staging"
+        )
+        
+        logger.debug(f'Model {model_name} version {model_version.version} registered and transitioned to Staging.')
+    except Exception as e:
+        logger.error('Error during model registration: %s', e)
+        raise
+
+def main():
+    try:
+        model_info_path = 'reports/run_info.json'
+        model_info = load_model_info(model_info_path)
+        
+        model_name = "Titanic Survival Predictor"
+        register_model(model_name, model_info)
+    except Exception as e:
+        logger.error('Failed to complete the model registration process: %s', e)
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    main()
